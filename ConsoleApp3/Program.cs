d497b2448bdcfd8a2a1214682f66362fbc574c67
kalyaniee1 <kalyaniraikar01@gmail.com>
2024-09-30 10:03:51 +0530

    ConsoleApp2

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+    internal class Program
+    {
+        static void Main()
+        {
+        int num = Convert.ToInt16(Console.ReadLine());
+        int first = 0, second = 1,next;
+        Console.WriteLine("Fibonacci series:");
+
+        for (int i = 0; i < num-2; i++)
+        {
+            if (i<=1)
+               next = i;
+            else
+            {
+                next = first + second;
+                first=second;
+                second = next;
+            }
+            Console.WriteLine(next);
+        }
+    }
+        
+    }
+
